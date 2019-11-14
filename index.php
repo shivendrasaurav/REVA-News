659e06a1a416623c319a81e4d45d9f5a84f04f29
Shivendra Saurav <shivendrasaurav@gmail.com>
2019-11-14 23:20:46 +0530

    Revamped Home Page

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://fluentdesignforweb.github.io/fluent.js"></script>
         <link href="https://fluentdesignforweb.github.io/fluent.css" type="text/css" rel="stylesheet">
-        <link href="https://shivendrasaurav.github.io/Fluent-Design-For-Web/Icons/css/icons.css" type="text/css" rel="stylesheet">
+        <link href="https://fluentdesignforweb.github.io/Icons/css/fluent-icons.css" type="text/css" rel="stylesheet">
         <link href="customstyles.css" type="text/css" rel="stylesheet">
         <style>
             .thumb{
@@ -19,46 +19,93 @@
         
     </head>
     <body>
-        <div class="column large10 medium0 small0 page_container center zi2">
-            <div class="pivot_container">
-                <img src="logo_reva.png" style="width: 40px; height: 40px; margin: -10px; margin-left: 5px;"/>
-                &nbsp;&nbsp;&nbsp;&nbsp;<span style="color: #000; font-size: 28px;">REVA News</span>
+
+    <div id="content">
+            <div class="primary_inverted pivot_container column large12 medium12 small12 zi2" style="position: fixed; width: 99%;">
+
+                <a class="pivot_button" style="font-size: 28px;" href="index.php">
+                    <img src="logo_reva.png" style="width: 40px; height: 40px; margin: -10px; margin-left: 5px;"/>
+                    &nbsp;&nbsp;&nbsp;&nbsp;REVA NEWS
+                </a>
+                <a class="pivot_button right" onclick="showsidepanel();">
+                    <i class="ms-Icon ms-Icon--GlobalNavButton" aria-hidden="true" style="color: #FFF;"></i>
+                </a>
+
+                </div>        
+                <div class="fsbg parallax" id="home">
+
+                    <div class="iam" style="margin-top: 100px; color: #FFF;">
+                        <h1 class="anim1">REVA NEWS</h1>
+                        <h3 class="anim2">Covering New Waters</h3>
+                    </div>
+                </div>
+                <div class=" page_container column large10 medium10 small12 center">
+                    <h5>Latest Articles</h5>
+                    <br><br>
+
+                    <p style='display: none'>After This</p> 
+
+        <div class='zi2 column large3 medium3 small12 left' style='margin: 10px;'>
+        <a class='tab' href='./This_Algorithm_Can_Remove_the_Water_from_Underwater_Photos,_and_the_Results_are_Incredible_Suhani_Jain_2019-11-14.html'>
+            <div>
+                <img class='thumb' src='./img_upload/The-Sea-thru-method-removes-water-from-underwater-images-Best-viewed-online-for-color.jpg'>
+                <br><br>
+                <span style='font-size: 28px; font-weight: 500'>This Algorithm Can Remove the Water from Underwater Photos, and the Results are Incredible</span> 
+                <br> 
+                Suhani Jain, 2019-11-14
             </div>
+        </a>
         </div>
-        <div class="column large10 medium10 small12 page_container center">
-            <h5>Latest Articles</h5>
-            <br><br>
 
-            <p style='display: none'>After This</p>
+        <div class='zi2 column large3 medium3 small12 left' style='margin: 10px;'>
+        <a class='tab' href='./TEDx_Talk_Josh_Kaufman_Shivendra_Saurav_2019-11-14.html'>
+            <div>
+                <img class='thumb' src='./img_upload/ted.jpg'>
+                <br><br>
+                <span style='font-size: 28px; font-weight: 500'>TEDx Talk Josh Kaufman</span> 
+                <br> 
+                Shivendra Saurav, 2019-11-14
+            </div>
+        </a>
+        </div>
 
-<div class='zi2 column large3 medium3 small12 left' style='margin: 10px;'>
-<a class='tab' href='./Microsoft_Rolls-Out_Windows_10_November_Update:_Here_is_Everything_You_Need_to_Know_Shivendra_Saurav_2019-11-14.html'>
-    <div>
-        <img class='thumb' src='./img_upload/windows-10-.jpg'>
-        <br><br>
-        <span style='font-size: 28px; font-weight: 500'>Microsoft Rolls-Out Windows 10 November Update: Here is Everything You Need to Know</span> 
-        <br> 
-        Shivendra Saurav, 2019-11-14
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    
+
+
+
+                </div>
+                <br><br>
+                <div class="primary_inverted column large12 medium12 small12">
+                    Used Fluent Design Design For Web To Develop This Website
+                </div>
     </div>
-</a>
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-            
-
-
-
+    <div class="sidepanel_container">
+    <div class="sidepanel_content zi3 right" id="modct1">
+        <button class="primary_red right ta_center mod_close_btn" onclick="closeside1();">X</button>
+        <div class="page_container">
+            <div class="large12 panel_item">Panel Item 1</div>
+            <div class="large12 panel_item">Panel Item 2</div>
+            <div class="large12 panel_item">Panel Item 3</div>
+            <div class="large12 panel_item">Panel Item 4</div>
+            <div class="large12 panel_item">Panel Item 5</div>
+            <div class="large12 panel_item">Panel Item 6</div>
         </div>
+    </div>
+    </div>
     </body>
 </html>
